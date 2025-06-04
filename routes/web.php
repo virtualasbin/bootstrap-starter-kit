7c7ad20d8e01febfeccd6aee455dd2fccd2c4e4d
Asbin Pandey <asbinpandey006@gmail.com>
2025-06-04 23:30:28 +0100

    Redirect home route to dashboard instead of displaying welcome view

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 })->name('home');
 
 Route::get('dashboard', \App\Livewire\Dashboard::class)
